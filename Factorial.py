77bb9272bab2346751608e280261e3ea4e5b1318
aksharaashok07-lgtm <aksharaashok07@gmail.com>
2026-06-24 12:49:06 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,5 @@
+factorial=1
+n=int(input("enter the number you want the factorial of:"))
+for i in range(1,n+1):
+    factorial=factorial*i
+print("The factorial of the given number is:", factorial)
